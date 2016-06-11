5610be86a347d77ade7d449f98fd74101905c689
Jordan Elver <jordan.elver@gmail.com>
2016-06-11 12:21:54 +0100

    Add unique constraint on `original_title` field
    This means we won't get duplicate movies when we do the import. The
    constraint will cause the insert to fail silently. We don't check any
    status when inserting, I'm not sure it matters in this instance.

--- db/structure.sql
+++ db/structure.sql
@@ -1,6 +1,6 @@
 create table movies (
   id serial primary key not null,
-  original_title text not null,
+  original_title text unique not null,
   title text not null,
   year text not null,
   release_date timestamptz,
