b697fb3ea1d899bda878792dfbba8b70e61950f9
Jordan Elver <jordan.elver@gmail.com>
2016-06-19 12:45:51 +0200

    Output real release dates from the database

--- lib/letterboxd_cal/calendar.ex
+++ lib/letterboxd_cal/calendar.ex
@@ -1,18 +1,67 @@
 defmodule LetterboxdCal.Calendar do
+  defmodule Item do
+    defstruct title: nil, dtstamp: nil, dtstart: nil, dtend: nil
+
+    def dtstamp(movie) do
+      long_date(movie.release_date)
+    end
+
+    def dtstart(movie) do
+      short_date(movie.release_date)
+    end
+
+    def dtend(movie) do
+      short_date(movie.release_date)
+    end
+
+    # TODO Rename to a sensible name
+    # What sort of date is this?
+    defp long_date(datetime) do
+      {_, datetime} = datetime |> Timex.parse("{YYYY}-{M}-{D}")
+
+      datetime
+      |> Timex.format("%Y%m%dT000000Z", :strftime)
+      |> elem(1)
+    end
+
+    defp short_date(datetime) do
+      {_, datetime} = datetime |> Timex.parse("{YYYY}-{M}-{D}")
+
+      datetime
+      |> Timex.format("%Y%m%d", :strftime)
+      |> elem(1)
+    end
+  end
+
+  import Moebius.Query
+  require EEx
+
+  EEx.function_from_file(:def, :template,
+    "lib/letterboxd_cal/calendar.eex", [:movies], trim: true)
+
   def ical do
-    """
-BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//Jordan Elver/LetterboxdCal//EN
-CALSCALE:GREGORIAN
-BEGIN:VEVENT
-DTSTAMP:20160322T140852Z
-DTSTART;VALUE=DATE:20160605
-DTEND;VALUE=DATE:20160605
-SUMMARY:Today
-END:VEVENT
-END:VCALENDAR
-"""
+    template(movies)
+  end
+
+  def movies do
+    Enum.map(movie_release_dates, fn (movie) ->
+      %Item{
+        title:   movie.title,
+        dtstamp: Item.dtstamp(movie),
+        dtstart: Item.dtstart(movie),
+        dtend:   Item.dtend(movie),
+      }
+    end)
+  end
+
+  def movie_release_dates do
+    db(:movies)
+    |> filter("release_date >= '#{today}'")
+    |> LetterboxdCal.Db.run
+  end
+
+  def today do
+    Timex.Date.today |> Timex.format("%Y-%m-%d", :strftime) |> elem(1)
   end
 end
 
